f82e89136a628cd281ad0d96c36ac0ed8fe1cc1b
Asan Azimkulov <azimkulov40@gmail.com>
2022-06-26 12:52:17 +0600

    file manage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,26 +5,25 @@ import qs from 'qs';
 import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import { Status } from '../redux/pizza/types';
-import { Sort as ISort } from '../redux/filter/types'
+import { Sort as ISort } from '../redux/filter/types';
 import { FilterSliceState } from '../redux/filter/types';
-import {selectFilter} from '../redux/filter/selectors';
-import {  setCategoryId,setCurrentPage,setFilters} from "../redux/filter/slice"
+import { selectFilter } from '../redux/filter/selectors';
+import { setCategoryId, setCurrentPage, setFilters } from '../redux/filter/slice';
 import { useAppDispatch } from '../redux/store';
-import {selectPizzasData } from '../redux/pizza/selectors';
+import { selectPizzasData } from '../redux/pizza/selectors';
 import { fetchPizzas } from '../redux/pizza/asyncActions';
 import { AppContext } from '../Context.js';
 
-import {options} from "../Components"
-import {Card,Categories,Sort,Skeleton,CartItem,Pagination} from "../Components"
+import { options } from '../Components';
+import { Card, Categories, Sort, Skeleton, CartItem, Pagination } from '../Components';
 
-import  NotFound  from './NotFound';
+import NotFound from './NotFound';
 
-
-export const Home: React.FC  = () => {
+export const Home: React.FC = () => {
   const navigate = useNavigate();
   const dispatch = useAppDispatch();
   const { categoryId, sort, currentPage, searchValue } = useSelector(selectFilter);
-  
+
   const { status, items } = useSelector(selectPizzasData);
   const isMounted = React.useRef(false);
 
@@ -65,19 +64,19 @@ export const Home: React.FC  = () => {
     window.scrollTo(0, 0);
     if (window.location.search) {
       const params = qs.parse(window.location.search.substring(1));
-      const sort = options.find((obj) => obj.type === params.sortType)
+      const sort = options.find((obj) => obj.type === params.sortType);
       if (!sort || !params.categoryId || !params.searchValue || !currentPage) {
         navigate('/');
         return;
       }
       dispatch(
         setFilters({
-          searchValue: String(params.search) || "",
-          categoryId: Number( params.category),
-          sort: {title: sort.title, type : sort.type},
-          currentPage:Number(params.page),
-        }
-      ));
+          searchValue: String(params.search) || '',
+          categoryId: Number(params.category),
+          sort: { title: sort.title, type: sort.type },
+          currentPage: Number(params.page),
+        }),
+      );
     }
   }, []);
 
@@ -95,9 +94,7 @@ export const Home: React.FC  = () => {
             ? [...new Array(currentPage > 2 ? 2 : 4)].map((el, index) => {
                 return <Skeleton key={index} />;
               })
-            : items.map((item) => (                
-                  <Card {...item}  key={item.id}/>        
-              ))}
+            : items.map((item) => <Card {...item} key={item.id} />)}
         </div>
         {status === Status.SUCCESS && items.length === 0 && (
           <NotFound description="К сожалению пицц не найдено" />
@@ -112,4 +109,4 @@ export const Home: React.FC  = () => {
       <Pagination />
     </>
   );
-}
+};
